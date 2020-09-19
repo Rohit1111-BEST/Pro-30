1de681e2598ad0f78f8215c9f7f16c6ee2237300
Rohit1111-BEST <65074708+Rohit1111-BEST@users.noreply.github.com>
2020-09-19 11:23:39 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -6,10 +6,9 @@ class Box {
       'density':1.0
     }
     this.body = Bodies.rectangle(x,y,width,height,options);
-    this.image= loadImage("box.png")
     this.width = width;
     this.height = height;
-    this.image = loadImage("sprites/base.png");
+    this.image = loadImage("box.png");
     World.add(world, this.body);
   }
 
@@ -20,19 +19,20 @@ class Box {
     rectMode(CENTER);
     fill("brown");
     rect(pos.x, pos.y, this.width, this.height);
+    imageMode(CENTER);
+    image(this.image,this.body.position.x,this.body.position.y,50,50);
     }
     else {
       World.remove(world,this.body);
       push()
       this.visibility=this.visibility-5
     tint(255,this.visibility)
+    imageMode(CENTER);
       image(this.image,this.body.position.x,this.body.position.y,50,50);
       
       pop ()
     }
 
-    imageMode(CENTER);
-    image(this.image, 0, 0, this.width, this.height);
     
   
     
